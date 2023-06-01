b8a115a01525b6fa20769d9657de044922971c1e
Maxwell Ramsdell <madramsdell@gmail.com>
2023-06-01 09:23:18 -0600

    Tells you that git added the files

--- nvim/.config/nvim/after/plugin/fugitive.lua
+++ nvim/.config/nvim/after/plugin/fugitive.lua
@@ -1,4 +1,7 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-vim.keymap.set("n", "<leader>ga", function() vim.cmd [[ :Git add . ]] end);
+vim.keymap.set("n", "<leader>ga", function() 
+  vim.cmd [[ :Git add . ]] 
+  print("Added Files")
+end);
 vim.keymap.set("n", "<leader>gc", function() vim.cmd [[ :Git commit ]] end);
 vim.keymap.set("n", "<leader>gp", function() vim.cmd [[ :Git push ]] end);
