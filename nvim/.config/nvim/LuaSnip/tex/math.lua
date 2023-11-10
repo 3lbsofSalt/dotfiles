b1652b60882d830a905fb888072cb8103c01ec33
3lbsofSalt <max.ramsdell@protonmail.com>
2023-11-09 18:40:29 -0700

    Added some lua math snippets

--- nvim/.config/nvim/LuaSnip/tex/math.lua
+++ nvim/.config/nvim/LuaSnip/tex/math.lua
@@ -7,10 +7,22 @@ return {
     trig="ff",
     dscr="Expand ff into frac{}{}"
   }, {
-    t("\\frac{"),
-    i(1),
-    t("}{"),
-    i(2),
-    t("}")
-  })
+      t("\\frac{"),
+      i(1),
+      t("}{"),
+      i(2),
+      t("}")
+    }),
+  ls.snippet({
+    trig="x1tn",
+    dscr="Expand into x sub 1 to x sub n"
+  }, {
+      t("x_1 \\cdots x_n")
+    }),
+  ls.snippet({
+    trig="y1tn",
+    dscr="Expand into y sub 1 to y sub n"
+  }, {
+      t("y_1 \\cdots y_n")
+    })
 }
