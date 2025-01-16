4bee653251bd571e391b81c5c72eebe14882bcf0
3lbsofSalt <max.ramsdell@protonmail.com>
2025-01-15 19:37:15 -0700

    Got More of DWM configured

---
+++ GruvboxCastle_Theme/.config/dwm/theme.h
@@ -0,0 +1,10 @@
+#ifndef THEME_FILE
+#define THEME_FILE
+static const char norm_bg[]       = "#282828";
+static const char norm_border[]       = "#928374";
+static const char norm_fg[]       = "#d5c4a1";
+static const char sel_fg[]       = "#ebdbb2";
+static const char sel_bg[]        = "#458588";
+static const char sel_border[]        = "#458588";
+
+#endif
--- dwm-code/config.h
+++ dwm-code/config.h
@@ -1,22 +1,29 @@
+#if __has_include("../../.config/dwm/theme.h")
+  #include"../../.config/dwm/theme.h"
+#endif
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int borderpx  = 4;        /* border pixel of windows */
+static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+
+#ifndef THEME_FILE
+static const char norm_bg[]       = "#222222";
+static const char norm_border[]       = "#444444";
+static const char norm_fg[]       = "#bbbbbb";
+static const char sel_fg[]       = "#eeeeee";
+static const char sel_bg[]        = "#005577";
+static const char sel_border[]        = "#005577";
+#endif
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { norm_fg, norm_bg, norm_border },
+	[SchemeSel]  = { sel_fg, sel_bg,  sel_border  },
 };
 
 /* tagging */
@@ -58,7 +65,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *rofi[]  = { "rofi", "-show", "drun", "-show-emojis", NULL };
 
